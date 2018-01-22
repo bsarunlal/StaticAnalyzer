f63411e17e7621b6fcadaae8e1f07d47b72f6a22
Arunlal <arunlal.beri.sankarlal@kp.org>
2018-01-22 13:10:44 +0530

    First commit

---
+++ app/src/main/java/com/test/pwc/mydatabinding1/TemperatureData.java
@@ -0,0 +1,13 @@
+package com.test.pwc.mydatabinding1;
+
+/**
+ * Created by aberisanka001 on 8/3/2017.
+ */
+
+public class TemperatureData extends BaseObserver{
+
+    @
+    public String location;
+    public int celcius;
+
+}
