64cf5c936ae85e9e1da28ee22dbc1494d45b6f50
Arunlal Beri Sankarlal <31500997+bsarunlal@users.noreply.github.com>
2018-03-16 14:55:36 +0530

    Update LoginActivity.java

--- app/src/main/java/com/webview/arun/helloworldapp/LoginActivity.java
+++ app/src/main/java/com/webview/arun/helloworldapp/LoginActivity.java
@@ -64,6 +64,12 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
     private boolean isUnsed;
 
+    //nEW METHOD added
+    public void M1() {
+        int i =0;
+        int j=0;
+    }
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
